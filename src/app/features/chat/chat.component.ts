6d91cd0e5a9df92aab714b42c72e8f6f879b8608
dev.vicentefigueroa <dev.vicentefigueroa@gmail.com>
2024-06-29 15:59:36 +0000

    Cambios en los formularios

--- src/app/features/chat/chat.component.ts
+++ src/app/features/chat/chat.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpHeaders,
+} from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import gsap from 'gsap';
@@ -12,38 +16,44 @@ gsap.registerPlugin(ScrollTrigger);
   standalone: true,
   imports: [HttpClientModule, CommonModule, FormsModule],
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.css'
+  styleUrl: './chat.component.css',
 })
 export class ChatComponent implements OnInit {
   message!: string;
+  email!: string;
   question!: string; // Agregamos una variable para almacenar la pregunta del usuario
   conversation: string[] = []; // Agregamos un array para almacenar la conversación
   loading = true;
+  messageCount = 0; // Agregamos una variable para contar los mensajes enviados
+  showPopup = false; // Variable para controlar la visibilidad del popup
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {    // Animation
-    gsap.fromTo('.chat',
-      { opacity: 0, }, // Estado inicial: transparente y ligeramente desplazado hacia arriba
+  ngOnInit(): void {
+    // Animation
+    gsap.fromTo(
+      '.chat',
+      { opacity: 0 }, // Estado inicial: transparente y ligeramente desplazado hacia arriba
       {
         opacity: 1, // Estado final: opacidad total
         y: '0%', // Volver a la posición original en Y
         duration: 3,
         scrollTrigger: {
-          trigger: ".projects", // El trigger ahora es la propia sección ".projects"
-          start: "top 80%",
-          toggleActions: "play none none reverse"
-        }
+          trigger: '.projects', // El trigger ahora es la propia sección ".projects"
+          start: 'top 80%',
+          toggleActions: 'play none none reverse',
+        },
       }
     );
     this.getInitialMessage(); // Llamamos a la función para obtener el mensaje inicial
   }
   getInitialMessage() {
     const headers = new HttpHeaders({
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     });
 
-    this.http.get('https://apiv-pi.vercel.app/chat', { headers: headers })
+    this.http
+      .get('https://apiv-pi.vercel.app/chat', { headers: headers })
       .subscribe((response: any) => {
         this.message = response.message.replace(/```/g, '').replace(/\n/g, '');
         this.conversation.push(this.message); // Agregamos el mensaje inicial a la conversación
@@ -54,23 +64,53 @@ export class ChatComponent implements OnInit {
 
   askQuestion() {
     const headers = new HttpHeaders({
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     });
     if (this.question) {
       const body = { question: this.question }; // Creamos un objeto con la pregunta del usuario
 
-      this.http.post('https://apiv-pi.vercel.app/chat', body, { headers: headers })
+      this.http
+        .post('https://apiv-pi.vercel.app/chat', body, { headers: headers })
         .subscribe((response: any) => {
-          this.message = response.message.replace(/```/g, '').replace(/\n/g, '');
+          this.message = response.message
+            .replace(/```/g, '')
+            .replace(/\n/g, '');
           this.conversation.push(`Tu: ${this.question}`); // Agregamos la pregunta del usuario a la conversación
           this.conversation.push(this.message); // Agregamos la respuesta del backend a la conversación
           console.log(this.message);
 
-          this.question = ''
+          this.question = '';
+          this.messageCount++; // Incrementamos el contador de mensajes
+          console.log(this.messageCount);
+
+          if (this.messageCount >= 2) {
+            this.showPopup = true; // Mostramos el popup después de 3 mensajes
+          }
         });
     }
   }
+  closePopup() {
+    if (this.email) {
+      const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+      });
+      const body = { email: this.email };
+
+      this.http
+        .post('https://apiv-pi.vercel.app/send-email', body, {
+          headers: headers,
+        })
+        .subscribe(
+          (response: any) => {
+            console.log('Correo enviado:', response);
+            this.email = ''; // Limpiar el campo de correo después de enviarlo
+            this.showPopup = false; // Cerrar el popup después de enviar el correo
+          },
+          (error) => {
+            console.error('Error al enviar el correo:', error);
+            // Manejar el error de acuerdo a tus necesidades
+          }
+        );
+    }
+  }
 }
-
-
-
--- src/app/features/contact/contact.component.ts
+++ src/app/features/contact/contact.component.ts
@@ -1,5 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import gsap from 'gsap';
@@ -13,82 +17,99 @@ gsap.registerPlugin(ScrollTrigger);
 
   imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
 export class ContactComponent {
-
   contact = {
     name: '', // y otros campos que necesites,
-    issue: '',// y otros campos que necesites
-    message: '' // y otros campos que necesites
+    issue: '', // y otros campos que necesites
+    message: '', // y otros campos que necesites
+    mail: '', // y otros campos que necesites
   };
   submitted = false;
   showSuccessMessage = false;
   name!: string;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
   ngOnInit() {
     // Animation
     const tl = gsap.timeline();
-    tl.fromTo('.tittle',
+    tl.fromTo(
+      '.tittle',
       { opacity: 0, y: '-10%' }, // Estado inicial: transparente y ligeramente desplazado hacia arriba
       {
         opacity: 1, // Estado final: opacidad total
         y: '0%', // Volver a la posición original en Y
         duration: 3,
         scrollTrigger: {
-          trigger: ".projects", // El trigger ahora es la propia sección ".projects"
-          start: "top 80%",
-          toggleActions: "play none none reverse"
-        }
+          trigger: '.tittle', // El trigger ahora es la propia sección ".projects"
+          start: 'top 80%',
+          toggleActions: 'play none none reverse',
+        },
       }
     );
-    tl.fromTo('.form',
+    tl.fromTo(
+      '.form',
       { opacity: 0, y: '-10%' }, // Estado inicial: transparente y ligeramente desplazado hacia arriba
       {
         opacity: 1, // Estado final: opacidad total
         y: '0%', // Volver a la posición original en Y
         duration: 3,
         scrollTrigger: {
-          trigger: ".projects", // El trigger ahora es la propia sección ".projects"
-          start: "top 80%",
-          toggleActions: "play none none reverse"
-        }
-      },">-2"
+          trigger: '.form', // El trigger ahora es la propia sección ".projects"
+          start: 'top 80%',
+          toggleActions: 'play none none reverse',
+        },
+      },
+      '>-2'
     );
   }
   submitForm() {
     this.submitted = true;
-
-    // Aquí puedes agregar lógica adicional, como enviar los datos a un servidor.
-    // ...
+    // Verificar si algún campo está vacío
+    if (
+      !this.contact.name.trim() ||
+      !this.contact.mail.trim() ||
+      !this.contact.issue.trim() ||
+      !this.contact.message.trim()
+    ) {
+      alert('Por favor, completa todos los campos.');
+      return;
+    }
 
     // No restablezcas el objeto contact aquí si quieres que el nombre se siga mostrando en el HTML
     console.log(this.contact); // Muestra el nombre en la consola
     this.name = this.contact.name;
 
-
     const contactData = {
       name: this.contact.name,
       issue: this.contact.issue,
-      message: this.contact.message
+      message: this.contact.message,
+      mail: this.contact.mail, // y otros campos que necesites
     };
     const headers = new HttpHeaders({
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     });
-    this.http.post('https://apiv-pi.vercel.app/items', contactData, { headers: headers })
-      .subscribe(response => {
-        console.log(response);
-        // Maneja la respuesta del servidor aquí
-      }, error => {
-        console.error(error);
-        // Maneja el error aquí
-      });
+    this.http
+      .post('https://apiv-pi.vercel.app/items', contactData, {
+        headers: headers,
+      })
+      .subscribe(
+        (response) => {
+          console.log(response);
+          // Maneja la respuesta del servidor aquí
+        },
+        (error) => {
+          console.error(error);
+          // Maneja el error aquí
+        }
+      );
 
     this.contact = {
       name: '', // y otros campos que necesites,
-      issue: '',// y otros campos que necesites
-      message: '' // y otros campos que necesites
+      issue: '', // y otros campos que necesites
+      message: '', // y otros campos que necesites
+      mail: '', // y otros campos que necesites
     }; // Esto limpiaría el formulario
     // Muestra el mensaje de éxito
     this.showSuccessMessage = true;
@@ -99,5 +120,4 @@ export class ContactComponent {
     }, 5000); // Oculta después de 3 segundos, por ejemplo
     this.submitted = false; // Esto ocultaría los datos en el HTML
   }
-
 }
