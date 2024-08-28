ac8295e53eb10f173523b12906d69a29291471f1
Vicente Figueroa <dev.vicentefigueroa@gmail.com>
2024-08-28 19:44:57 -0400

    primeros cambios en el home

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,30 +1,18 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-import gsap from 'gsap'; 
+import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
+import { ProjectsComponent } from '../projects/projects.component';
 
 gsap.registerPlugin(ScrollTrigger);
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ProjectsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   ngOnInit() {
-    gsap.fromTo('.aboutme', 
-      { opacity: 0, y: '-10%' }, // Estado inicial: transparente y ligeramente desplazado hacia arriba
-      {
-        opacity: 1, // Estado final: opacidad total
-        y: '0%', // Volver a la posición original en Y
-        duration: 3,
-        scrollTrigger: {
-          trigger: ".aboutme", // El trigger ahora es la propia sección ".aboutme"
-          start: "top 80%", 
-          toggleActions: "play none none reverse" 
-        }
-      }
-    );
   }
 }
