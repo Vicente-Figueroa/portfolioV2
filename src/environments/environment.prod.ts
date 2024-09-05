2fb9763269fe5ab545e28b3cc7d31c9aca36fbde
Vicente Figueroa <dev.vicentefigueroa@gmail.com>
2024-09-05 17:00:35 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://apiv-pi.vercel.app/' // URL del backend en producción
+    apiUrl: 'https://apiv-pi.vercel.app' // URL del backend en producción
   };
