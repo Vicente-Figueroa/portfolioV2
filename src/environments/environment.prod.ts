6cf0c44220956e93b6c1f840d7d1a3b5266286e4
Vicente Figueroa <dev.vicentefigueroa@gmail.com>
2024-09-14 23:37:30 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://portfolio-api-django-1-db9c603437c8.herokuapp.com' // URL del backend en producción
+    apiUrl: 'https://portafolio-django-api.onrender.com' // URL del backend en producción
   };
