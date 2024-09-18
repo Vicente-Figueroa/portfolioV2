23ee449769549efa0ef6e20696bdc21f833cbccc
Vicente Figueroa <dev.vicentefigueroa@gmail.com>
2024-09-18 00:49:43 -0300

    cambios en el nav

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -11,31 +11,25 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-  public debugMode: boolean = false; // Estado del modo debug
+  public debugMode: boolean = false;
 
   ngOnInit() {
-    // Verificar si el modo debug existe en localStorage
     const savedDebugMode = localStorage.getItem('navDebugMode');
-
-    // Si no existe, inicializarlo en false
     if (savedDebugMode === null) {
       localStorage.setItem('navDebugMode', 'false');
       this.debugMode = false;
     } else {
-      // Si existe, sincronizar el valor con el estado del componente
       this.debugMode = savedDebugMode === 'true';
     }
   }
 
   toggleNavDebugMode(newValue: boolean) {
-    // Guardar el nuevo estado en localStorage
     localStorage.setItem('navDebugMode', newValue.toString());
   }
 
-  // Función para eliminar el token de onboarding
   resetOnboarding() {
     localStorage.removeItem('hasSeenOnboarding');
     alert('¡Se recargará la página!');
-    window.location.reload(); // Recargar la página automáticamente
+    window.location.reload();
   }
 }
